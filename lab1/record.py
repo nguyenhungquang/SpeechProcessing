5735467635def4bf3fecf55b2cf18d00e6e812b0
nguyenhungquang <grisaia9690@gmail.com>
2020-03-30 17:49:29 +0700

    ver1

--- lab1/record.py
+++ lab1/record.py
@@ -1,4 +1,8 @@
 import argparse
+import re
+import nltk
+import subprocess
+import os
 import tempfile
 import queue
 import sys
@@ -7,9 +11,10 @@ import sounddevice as sd
 import soundfile as sf
 import numpy  # Make sure NumPy is loaded before it is used in the callback
 assert numpy  # avoid "imported but unused" message (W0611)
+#nltk.download('all')
 start=False
 stop=False
-
+directory=""
 def int_or_str(text):
     """Helper function for argument parsing."""
     try:
@@ -30,6 +35,9 @@ parser = argparse.ArgumentParser(
     description=__doc__,
     formatter_class=argparse.RawDescriptionHelpFormatter,
     parents=[parser])
+parser.add_argument(
+    'dir',nargs='?',metavar='1',
+    help='directory name')
 parser.add_argument(
     'filename', nargs='?', metavar='FILENAME',
     help='audio file to store recording to')
@@ -74,7 +82,7 @@ def start_recording(index):
         if args.filename is None:
             args.filename = tempfile.mktemp(prefix=str(index)+'sentence',
                                             suffix='.wav', dir='')
-        new_file= tempfile.mktemp(prefix=str(index)+'sentence',suffix='.wav', dir='')
+        new_file= tempfile.mktemp(prefix=str(index)+'sentence',suffix='.wav', dir=args.dir)
         # Make sure the file is opened before recording anything:
         with sf.SoundFile(new_file, mode='x', samplerate=args.samplerate,
                           channels=args.channels, subtype=args.subtype) as file:
@@ -88,22 +96,43 @@ def start_recording(index):
     except KeyboardInterrupt:
         print('\nRecording finished: ' + repr(new_file))
         #break
+        return new_file
         #parser.exit(0)
     except Exception as e:
         parser.exit(type(e).__name__ + ': ' + str(e))
-def record_multi_files():
+def record_multi_files(array):
     index=0
     while True:
         is_rec=input("Start recording? y/n? ")
         if is_rec=='y':
-            start_recording(index)
+            while len(re.split(r'[a-zA-Z0-9]',array[index]))==0:
+                array.pop(index)
+            print(array[index])
+            wav_name=start_recording(index)
+            with open("./"+args.dir+"/index.txt",'a',encoding="utf-8") as f:
+                f.write(wav_name[wav_name.find("\\")+1:].split()[0]+"\n")
+                f.write(array[index]+"\n")
         if is_rec=='n':
             return
         index+=1
 if __name__=='__main__':
-    
+    txt_file="text.txt"
+    subprocess.Popen("notepad.exe "+txt_file)
+    if args.dir is None:
+        args.dir="1"
+    try:
+        os.mkdir(args.dir)
+    except OSError:
+        print("Directory already exist")
+        sys.exit()
+    with open(txt_file,encoding="utf-8") as f:
+        file_content=f.read()
+    #sentence_array=re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s',file_content)
+    sentence_array=nltk.tokenize.sent_tokenize(file_content)
+    #print(sentence_array)
     #with keyboard.Listener(
     #        on_press=on_press,
     #        on_release=on_release) as listener:
     #    listener.join()
-    record_multi_files()
+    record_multi_files(sentence_array)
+
