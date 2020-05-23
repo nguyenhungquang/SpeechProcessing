b5a812becff88fef70e0c2e3a58d11334ebc7acb
nguyenhungquang <grisaia9690@gmail.com>
2020-05-23 23:17:44 +0700

    add reports

--- hmm_word/wordreg.py
+++ hmm_word/wordreg.py
@@ -6,12 +6,21 @@ from sklearn.cluster import KMeans
 import sklearn
 import hmmlearn.hmm
 import pickle
-def get_class_data(data_dir):
+import record
+import soundfile as sf
+def get_class_data(data_dir,augmentation=False):
     files = os.listdir(data_dir)
     mfcc = [get_mfcc(os.path.join(data_dir,f)) for f in files if f.endswith(".wav")]
+    if augmentation:
+        mfcc=np.random.choice(mfcc,len(mfcc)//2).tolist()
     return mfcc
-def get_mfcc(file_path):
+def get_mfcc(file_path,norm=False):
     y, sr = librosa.load(file_path) # read .wav file
+    if norm:
+        if len(y)//2>9:
+            y=y[len(y)//2:]
+        y=librosa.effects.time_stretch(y, 2.0)
+        sf.write(file_path,y,sr)
     hop_length = math.floor(sr*0.010) # 10ms hop
     win_length = math.floor(sr*0.025) # 25ms frame
     # mfcc is 12 x T matrix
@@ -36,14 +45,14 @@ class_names = ["ngay","nhieu","nhung","gia_dinh","dich_benh"]
 test_classes=["test_ngay","test_nhieu","test_nhung","test_gia_dinh","test_dich_benh"]
 dataset = {}
 testset={}
-def get_data(class_list):
+def get_data(class_list,train=False):
     dt={}
     for cname in class_list:
         print(f"Load {cname}")
         dt[cname] = get_class_data(os.path.join("data", cname))
+        if train:
+            dt[cname]+=get_class_data(os.path.join("augmentation", cname),augmentation=True)
     return dt
-dataset=get_data(class_names)
-testset=get_data(test_classes)
 word_models={"ngay":824,"nhieu":811,"nhung":1145,"gia_dinh":215,"dich_benh":216}
 for name in word_models.keys():
     word_models[name]=np.log(word_models[name])
@@ -59,7 +68,8 @@ def start_prior(size):
     pri[0]=pri[1]=0.5
     return pri
 def train():
-    components_dict=dict(zip(class_names,[9,9,9,18,18]))
+    dataset=get_data(class_names,train=True)
+    components_dict=dict(zip(class_names,[9,9,9,15,18]))
     models={}
     for cname in class_names:
         #if cname[:4] != 'test':
@@ -83,8 +93,10 @@ def train():
         models[cname] = hmm
     return models
 def test(models):
+    testset=get_data(test_classes)
     print("Testing")
     f=0
+    errors_dict=dict(zip(class_names,[0,0,0,0,0]))
     for name in test_classes:
         for O in testset[name]:
             true_cname=name[5:]
@@ -92,11 +104,23 @@ def test(models):
             inverse = [(value, key) for key, value in score.items()]
             pre = max(inverse)[1]
             if true_cname!=pre:
+                errors_dict[true_cname]+=1
                 f+=1
     print("Error:", f,"%")
+    print(errors_dict)
+def predict(f,models):
+    features=get_mfcc(f,norm=True)
+    score = {cname : (model.score(features, [len(features)])+word_models[cname]) for cname, model in models.items() }
+    inverse = [(value, key) for key, value in score.items()]
+    pre = max(inverse)[1]
+    return pre
+        
 if __name__=="__main__":
     load=True
     save=False
+    is_load=input("Load model? Y/n? If not, train new model!")
+    if is_load=='n':
+        load=False
     models={}
     if not load:
         models=train()
@@ -105,7 +129,16 @@ if __name__=="__main__":
             with open(name+".pkl", "rb") as file: 
                 models[name]=pickle.load(file)
     test(models)
+    is_save=input("Save model? y/n?")
+    if is_save=='y':
+        save=True
     if save:
         for name in class_names:
             with open(name+".pkl", "wb") as file: pickle.dump(models[name], file)
-
+    while True:
+        direct_test=input("Directly test? y/n?")
+        if direct_test=='y':
+            f=record.start_recording(1,d="test")
+            print(predict(f,models))
+        else:
+            break
